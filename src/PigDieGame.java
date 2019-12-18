3ee61294810a10f1f00e200818de900a6da948a6
Katarina Pejcinovic <pejcinok@wlhs.wlwv.k12.or.us>
2019-12-17 17:15:26 -0800

    Added new method to Game. Fixed total score thing

--- src/PigDieGame.java
+++ src/PigDieGame.java
@@ -16,10 +16,13 @@ public class PigDieGame {
         while (player1.getScore() < 100 && player2.getScore() < 100) {
             System.out.println("Player 1's turn:");
             playerTurn(player1);
+
             System.out.println("Player 2's turn:");
             playerTurn(player2);
+            score(player2);
 
         }
+
 
         if (player1.getScore() >= 100) {
             System.out.println("Player 1 wins!");
@@ -29,43 +32,44 @@ public class PigDieGame {
 
     }
 
+        private void playerTurn (PigDiePlayer player){
+            String answer = "r";
 
-    private void playerTurn(PigDiePlayer player) {
-        String answer = "r";
-        int newScore = 0;
-        while (answer.equals("r")) {
-            System.out.println("Would you like to roll or pass? (r/p)");
-            answer = scan.nextLine();
-            if (answer.equals("r")) {
+            while (answer.equals("r")) {
+                System.out.println("Would you like to roll or pass? (r/p)");
+                answer = scan.nextLine();
+                if (answer.equals("r")) {
 
-                player.rollDice();
+                    player.rollDice();
+                    score(player);
 
-                if (player.getRoll1() == 1 && player.getRoll2() == 1) {
-                    System.out.println("Yikes! Snake Eyes. Total score reset.");
-                    newScore = 0;
-                    answer = "p";
-                    System.out.println("Round Score: " + player.getScore());
-
-                } else if (player.getRoll1() == 1 || player.getRoll2() == 1) {
-                    System.out.println("Uh oh you rolled a one. Round score reset.");
-                    newScore = player.getScore();
-                    answer = "p";
-                    System.out.println("Round Score: 0");
-
-                } else {
-
-                    newScore = player.getRoll1() + player.getRoll2();
-                    System.out.println("Round score: "+ newScore);
-                    player.setScore(newScore);
-                    System.out.println("Total score:" + player.getScore());
-
-
-
-                    }
                 }
-
             }
 
         }
+        private void score(PigDiePlayer player){
+            int newScore = 0;
+            if (player.getRoll1() == 1 && player.getRoll2() == 1) {
+                System.out.println("Yikes! Snake Eyes. Total score reset.");
+                System.out.println("Round Score: " + player.getScore());
+
+            }
+            if (player.getRoll1() == 1 || player.getRoll2() == 1) {
+                System.out.println("Uh oh you rolled a one. Round score reset.");
+                newScore = 0;
+                System.out.println("Round Score: 0");
+
+            }
+            else{
+                newScore = player.getRoll1() + player.getRoll2();
+                System.out.println("Round score: " + newScore);
+
+
+            }
+
+            player.setScore((player.getScore() + newScore));
+            System.out.println("Total score:" + player.getScore());
+        }
+
 
 }
